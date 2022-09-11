10c1d991063d3719cd7a2cd2cb5f0f23edb2bb68
Joshua Simmons <josh@nega.tv>
2022-09-11 22:57:03 +0200

    Add stb_image binding

---
+++ ffi/stb_image-sys/build.rs
@@ -0,0 +1,30 @@
+use std::{path::Path, process::Command};
+
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let opt_level = std::env::var("OPT_LEVEL").unwrap();
+
+    Command::new("clang")
+        .args(&[
+            "src/stb_image.c",
+            "-c",
+            &format!("-O{opt_level}"),
+            "-fPIC",
+            "-o",
+        ])
+        .arg(&format!("{out_dir}/stb_image.o"))
+        .status()
+        .unwrap();
+
+    Command::new("llvm-ar")
+        .args(&["crus", "libstb_image.a", "stb_image.o"])
+        .current_dir(&Path::new(&out_dir))
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={out_dir}");
+    println!("cargo:rustc-link-lib=static=stb_image");
+    println!("cargo:rerun-if-changed=src/stb_image.c");
+    println!("cargo:rerun-if-changed=src/stb_image.h");
+    println!("cargo:rerun-if-changed=build.rs");
+}
---
+++ ffi/stb_image-sys/src/lib.rs
@@ -0,0 +1,173 @@
+use std::os::raw::{c_char, c_float, c_int, c_uchar, c_void};
+
+use libc::FILE;
+
+#[repr(C)]
+pub struct stbi_io_callbacks {
+    /// fill 'data' with 'size' bytes.  return number of bytes actually read
+    pub read: extern "C" fn(user: *mut c_void, data: *mut c_char, size: c_int) -> i32,
+    /// skip the next 'n' bytes, or 'unget' the last -n bytes if negative
+    pub skip: extern "C" fn(user: *mut c_void, n: c_int),
+    /// returns nonzero if we are at end of file/data
+    pub eof: extern "C" fn(user: *mut c_void) -> i32,
+}
+
+extern "C" {
+    pub fn stbi_load_from_memory(
+        buffer: *const c_uchar,
+        len: c_int,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_uchar;
+
+    pub fn stbi_load(
+        filename: *const c_char,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_uchar;
+
+    pub fn stbi_load_from_file(
+        f: *mut FILE,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_uchar;
+
+    pub fn stbi_load_from_callbacks(
+        clbk: *const stbi_io_callbacks,
+        user: *mut c_void,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_uchar;
+
+    pub fn stbi_loadf_from_memory(
+        buffer: *const c_uchar,
+        len: c_int,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_float;
+
+    pub fn stbi_loadf(
+        filename: *const c_char,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_float;
+
+    pub fn stbi_loadf_from_file(
+        f: *mut FILE,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_float;
+
+    pub fn stbi_loadf_from_callbacks(
+        clbk: *const stbi_io_callbacks,
+        user: *mut c_void,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+        req_comp: c_int,
+    ) -> *mut c_float;
+
+    pub fn stbi_hdr_to_ldr_gamma(gamma: c_float);
+
+    pub fn stbi_hdr_to_ldr_scale(scale: c_float);
+
+    pub fn stbi_ldr_to_hdr_gamma(gamma: c_float);
+
+    pub fn stbi_ldr_to_hdr_scale(scale: c_float);
+
+    pub fn stbi_is_hdr_from_callbacks(clbk: *const stbi_io_callbacks, user: *mut c_void) -> c_int;
+
+    pub fn stbi_is_hdr_from_memory(buffer: *const c_uchar, len: c_int) -> c_int;
+
+    pub fn stbi_is_hdr(filename: *const c_char) -> c_int;
+
+    pub fn stbi_is_hdr_from_file(f: *mut FILE) -> c_int;
+
+    pub fn stbi_failure_reason() -> *const c_char;
+
+    pub fn stbi_image_free(retval_from_stbi_load: *mut c_void);
+
+    pub fn stbi_info_from_memory(
+        buffer: *const c_uchar,
+        len: c_int,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+    ) -> c_int;
+
+    pub fn stbi_info_from_callbacks(
+        clbk: *const stbi_io_callbacks,
+        user: *mut c_void,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+    ) -> c_int;
+
+    pub fn stbi_info(
+        filename: *const c_char,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+    ) -> c_int;
+
+    pub fn stbi_info_from_file(
+        f: *mut FILE,
+        x: *mut c_int,
+        y: *mut c_int,
+        comp: *mut c_int,
+    ) -> c_int;
+
+    pub fn stbi_set_unpremultiply_on_load(flag_true_if_should_unpremultiply: c_int);
+
+    pub fn stbi_convert_iphone_png_to_rgb(flag_true_if_should_convert: c_int);
+
+    pub fn stbi_zlib_decode_malloc_guesssize(
+        buffer: *const c_char,
+        len: c_int,
+        initial_size: c_int,
+        outlen: *mut c_int,
+    ) -> *mut c_char;
+
+    pub fn stbi_zlib_decode_malloc(
+        buffer: *const c_char,
+        len: c_int,
+        outlen: *mut c_int,
+    ) -> *mut c_char;
+
+    pub fn stbi_zlib_decode_buffer(
+        obuffer: *const c_char,
+        olen: c_int,
+        ibuffer: *const c_char,
+        ilen: c_int,
+    ) -> c_int;
+
+    pub fn stbi_zlib_decode_noheader_malloc(
+        buffer: *const c_char,
+        len: c_int,
+        outlen: *mut c_int,
+    ) -> *mut c_char;
+
+    pub fn stbi_zlib_decode_noheader_buffer(
+        obuffer: *mut c_char,
+        olen: c_int,
+        ibuffer: *const c_char,
+        ilen: c_int,
+    ) -> c_int;
+
+    pub fn stbi_set_flip_vertically_on_load(flag_true_if_should_flip: i32);
+
+}
