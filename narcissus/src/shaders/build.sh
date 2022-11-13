32136d3d5ef2a74bc9a464cb0c9da350fdc6e554
Joshua Simmons <josh@nega.tv>
2022-11-13 22:50:35 +0100

    Simplify use of shader build script

--- narcissus/src/shaders/build.sh
+++ narcissus/src/shaders/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+pushd "${0%/*}"
 glslc --target-env=vulkan1.3 -O -fshader-stage=vert -o basic.vert.spv basic.vert.glsl
 glslc --target-env=vulkan1.3 -O -fshader-stage=frag -o basic.frag.spv basic.frag.glsl
+echo "built shaders"
+popd
