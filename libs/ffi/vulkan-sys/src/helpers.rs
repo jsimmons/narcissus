00013b87a356dcdce7b83be5db244d996f63014f
Joshua Simmons <josh@nega.tv>
2023-04-20 21:22:20 +0200

    misc: Improve cstr! macro implementation

--- libs/ffi/vulkan-sys/src/helpers.rs
+++ libs/ffi/vulkan-sys/src/helpers.rs
@@ -1,14 +1,9 @@
-#[allow(unconditional_panic)]
-const fn illegal_null_in_string() {
-    [][0]
-}
-
 #[doc(hidden)]
 pub const fn validate_cstr_contents(bytes: &[u8]) {
     let mut i = 0;
     while i < bytes.len() {
         if bytes[i] == b'\0' {
-            illegal_null_in_string();
+            panic!("illegal null byte in string");
         }
         i += 1;
     }
@@ -20,15 +15,9 @@ macro_rules! cstr {
         $crate::helpers::validate_cstr_contents($s.as_bytes());
         #[allow(unused_unsafe)]
         unsafe {
-            std::mem::transmute::<_, &std::ffi::CStr>(concat!($s, "\0"))
+            std::ffi::CStr::from_bytes_with_nul_unchecked(concat!($s, "\0").as_bytes())
         }
     }};
-}
-
-#[allow(dead_code)]
-pub fn string_from_c_str(c_str: &[i8]) -> String {
-    let s = unsafe { std::ffi::CStr::from_ptr(c_str.as_ptr()).to_bytes() };
-    String::from_utf8_lossy(s).into_owned()
 }
 
 #[cfg(test)]
